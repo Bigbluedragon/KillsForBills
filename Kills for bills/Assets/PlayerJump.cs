04fc0505630d979882ddb33f4c844b172e941aef
magekX <128644181+magekX@users.noreply.github.com>
2023-03-28 20:38:16 +0100

    movement
    added movement

---
+++ Kills for bills/Assets/PlayerJump.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [SerializeField] int jumpPower;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    bool isGrounded;
+
+
+    void Start()
+    {
+        rb = GetComponent <Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+
+        isGrounded = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(0.32f, 1f), CapsuleDirection2D.Vertical, 0, groundLayer);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+        
+    }
+}
---
+++ Kills for bills/Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private float moveDirection;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gets the input
+        moveDirection = Input.GetAxis("Horizontal");
+       
+
+            if (moveDirection > 0 && !facingRight)
+        {
+            FlipCharacter();
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            FlipCharacter();
+        }
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+       
+    }
+
+    private void FlipCharacter()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
